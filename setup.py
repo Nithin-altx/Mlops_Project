90c4b8f08fb0b59828ac88d043b52ee950dd7b27
Nithin <nithin200133@gmail.com>
2026-02-16 18:50:45 +0530

    mongodb,venv,eda completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nithin",
+    author_email="nithin200133@gmail.com",
+    packages=find_packages()
+)
